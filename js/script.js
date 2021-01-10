2a27e358f4f4c286b9fd0e4efe0b314ce4bcd92b
Maciej Suchecki <maciej@MacBook-Pro-Maciej.local>
2021-01-10 21:18:59 +0100

    add link search

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+/* wyszukanie wszystkich linków - test
+document.getElementById('test-button').addEventListener('click', function (){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+})
+*/
