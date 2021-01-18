c934af74246c2d3c9ef7b8162d1eadc977018042
Maciej Suchecki <maciej@MacBook-Pro-Maciej.local>
2021-01-18 22:28:41 +0100

    add active class

--- js/script.js
+++ js/script.js
@@ -6,3 +6,40 @@ document.getElementById('test-button').addEventListener('click', function (){
     console.log('links:', links);
 })
 */
+
+const titleClickHandler = function(event){
+    event.preventDefault();
+    console.log('Link was clicked');
+    console.log('event');
+
+/* <DONE> remove class 'active' from all article links */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+/* <DONE> add class 'active' to the clicked links */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+
+/* <DONE> remove class 'active' from all articles */
+    const activeArticle = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles){
+        activeLink.classList.remove('active');
+    }
+
+/* get 'href' attribute from the clicked links */
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+/* add class 'active' to the correct article */
+
+
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+}
